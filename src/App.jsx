d78eefc2f358b0e9116bbb256a30f9ff9fa6b197
James <james.mcgrory135@gmail.com>
2023-03-31 12:05:36 +1100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  )
+}
+
+export default App

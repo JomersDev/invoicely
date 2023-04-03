d47509d45d080bcc185a7eb37ac2fe730373cf3b
James <james.mcgrory135@gmail.com>
2023-04-03 17:32:44 +1000

    setup project colors and imported League Spartan font family

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div>
-      <h1>test</h1>
+      <h1 className='text-5xl'>test</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'violet': '#7C5DFA',
+        'light-violet': '#9277FF',
+        'navy': '#1E2139',
+        'light-navy': '#252945',
+        'Lavender': '#DFE3FA',
+        'cool-grey': '#888EB0',
+        'ube': '#7E88C3',
+        'chinese-black': '#0C0E16',
+        'red': '#EC5757',
+        'pink': '#FF9797',
+        'ghost-white': '#F8F8FB',
+        'eerie-black': '#141625'
+      },
+      fontFamily: {
+        'league-spartan' : ['League Spartan', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }

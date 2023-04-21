71c3b106effb6fed42dd525dbeb674286cc590c5
James McGrory <james.mcgrory135@gmail.com>
2023-04-21 15:10:52 +1000

    fixed bugs with file names and npm run dev not working correctly

--- src/Components/InvoiceList/NewInvoiceForm.jsx
+++ src/Components/InvoiceList/NewInvoiceForm.jsx
@@ -1,18 +1,21 @@
 export default function NewInvoiceForm(props) {
 
     return (
-        <div className="absolute min-h-screen bg-white top-10 right-28">
+        <div className="min-h-screen bg-white">
             <div 
                 onClick={props.handleShowNewInvoice}
-                className="flex items-center justify-start">
-                    <img src="/public/icon-arrow-left.svg" alt="arrow right icon" />
-                    <p className="mt-1 font-bold">
+                className="flex items-center justify-start gap-x-3">
+                    <img src="/icon-arrow-left.svg" alt="arrow right icon" />
+                    <p className="mt-1 font-bold cursor-pointer">
                         Go 
                     </p>
             </div>
             <h1>
                 New Invoice
             </h1>
+            <form>
+                <input type="text" placeholder="Description" />
+            </form>
         </div>
     )
 }
--- src/Pages/InvoiceList.jsx
+++ src/Pages/InvoiceList.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import NewInvoiceBtn from "../Components/Buttons/NewInvoiceBtn";
 import InvoiceCard from "../Components/InvoiceList/InvoiceCard";
 import Empty from "../Components/InvoiceList/Empty";
-import NewInvoiceForm from "../Components/InvoiceList/newInvoiceForm";
+import NewInvoiceForm from "../Components/InvoiceList/NewInvoiceForm";
 
 export default function InvoiceList(props) {
 
-    const [showNewInvoice, setShowNewInvoice] = useState(true)
+    const [showNewInvoice, setShowNewInvoice] = useState(false)
 
     function handleShowNewInvoice() {
         console.log('test')
@@ -15,42 +15,46 @@ export default function InvoiceList(props) {
 
     return (
         <section className="min-h-screen px-6 pt-8 bg-ghost-white md:px-12 lg:w-full lg:px-80">
-            <div className="flex items-center justify-between max-w-3xl mx-auto">
-                <div>
-                    <h1 className="text-2xl font-bold tracking-tighter text-chinese-black md:text-4xl">
-                        Invoices
-                    </h1>
-                    <p className="tracking-tight text-cool-grey">
-                        <span className="hidden md:inline-block">There are </span> {props.invoices.length} <span className="hidden md:inline-block">total </span> Invoices
-                    </p>
-                </div>
-                <div className="relative flex gap-x-5">
-                    <div className="flex items-center gap-x-3">
-                        <p className="font-bold tracking-tight text-chinese-black">
-                            Filter <span className="hidden md:inline-block">by status</span>
+            {showNewInvoice ? <NewInvoiceForm handleShowNewInvoice={handleShowNewInvoice}/>
+            :
+            <div>
+                <div className="flex items-center justify-between max-w-3xl mx-auto">
+                    <div>
+                        <h1 className="text-2xl font-bold tracking-tighter text-chinese-black md:text-4xl">
+                            Invoices
+                        </h1>
+                        <p className="tracking-tight text-cool-grey">
+                            <span className="hidden md:inline-block">There are </span> {props.invoices.length} <span className="hidden md:inline-block">total </span> Invoices
                         </p>
-                        <img src="/icon-arrow-down.svg" alt="down arrow" />
                     </div>
-                    <NewInvoiceBtn addInvoice={props.addInvoice} handleShowNewInvoice={handleShowNewInvoice}/>
-                    {showNewInvoice && <NewInvoiceForm handleShowNewInvoice={handleShowNewInvoice}/>}
+                    <div className="relative flex gap-x-5">
+                        <div className="flex items-center gap-x-3">
+                            <p className="font-bold tracking-tight text-chinese-black">
+                                Filter <span className="hidden md:inline-block">by status</span>
+                            </p>
+                            <img src="/icon-arrow-down.svg" alt="down arrow" />
+                        </div>
+                        <NewInvoiceBtn addInvoice={props.addInvoice} handleShowNewInvoice={handleShowNewInvoice}/>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center mt-8 gap-y-4 md:mt-14">
+                    {props.invoices.length === 0 ? <Empty/> :
+                        <>
+                            {props.invoices.map(invoice => {
+                                return <InvoiceCard 
+                                            key={invoice.id} 
+                                            id={invoice.id}
+                                            paymentDue={invoice.paymentDue}
+                                            clientName={invoice.clientName}
+                                            total={invoice.total}
+                                            status={invoice.status}
+                                        />
+                            })}
+                        </>
+                    }
                 </div>
             </div>
-            <div className="flex flex-col items-center mt-8 gap-y-4 md:mt-14">
-                {props.invoices.length === 0 ? <Empty/> :
-                    <>
-                        {props.invoices.map(invoice => {
-                            return <InvoiceCard 
-                                        key={invoice.id} 
-                                        id={invoice.id}
-                                        paymentDue={invoice.paymentDue}
-                                        clientName={invoice.clientName}
-                                        total={invoice.total}
-                                        status={invoice.status}
-                                    />
-                        })}
-                    </>
-                }
-            </div>
+            }
         </section>
     )
 }
